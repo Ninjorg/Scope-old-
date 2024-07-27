aeb54b56532777a1ca343c7923ce59ddcdc6efc8
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-07-27 08:57:05 -0700

    Update emailFinder1.js

--- emailFinder1.js
+++ emailFinder1.js
@@ -6,7 +6,7 @@ const options = {
 	port: null,
 	path: '/find-emails?name=Ronit%20Parikh&email_domain=gmail.com&min_similarity=0',
 	headers: {
-		'x-rapidapi-key': 'c753760241msh4aafaa1fda97f81p1b0a40jsnf163a2a6b34f',
+		'x-rapidapi-key': 'your-key',
 		'x-rapidapi-host': 'email-finder4.p.rapidapi.com'
 	}
 };
